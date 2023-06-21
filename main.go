b245f6d4430712bc4a5631e981795c69d3e0e2c9
seanperry <seanep94@gmail.com>
2023-06-20 20:58:44 -0700

    Working through w3s site before I hit the books 50% of the way

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+}
+
+/*
+	Finish working through this before you go into the books:
+	https://www.w3schools.com/go/go_output.php
+*/
