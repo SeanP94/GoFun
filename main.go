97fd2ea93c6a62d99f40301cd137cf524df51a28
seanperry <seanep94@gmail.com>
2023-06-21 21:25:53 -0700

    Playing with structs

--- main.go
+++ main.go
@@ -4,12 +4,41 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello World!")
-
+type Electronic interface {
+	drainPower()
+	killRobot()
 }
 
-/*
-	Finish working through this before you go into the books:
-	https://www.w3schools.com/go/go_output.php
-*/
+type Robot struct {
+	Name  string
+	Power int
+}
+
+/* Drains the robots power by 1 */
+func (r *Robot) drainPower() {
+	if r.Power != 0 {
+		r.Power--
+	} else {
+		fmt.Println("This poor robot! Can't you see you've already depleted its battery!?")
+		return
+	}
+
+	if r.Power == 0 {
+		fmt.Printf("WARNING!\n%v's power level has depleted!\n", r.Name)
+	} else {
+		fmt.Printf("%v's power level is %v.\n", r.Name, r.Power)
+	}
+}
+
+/* Kills the robot by depleting it's power to 0. */
+func (r *Robot) killRobot() {
+	currPower := r.Power
+	for i := 0; i < currPower; i++ {
+		r.drainPower()
+	}
+}
+
+func main() {
+	myRobot := Robot{"R2D2", 10}
+	myRobot.killRobot()
+}
