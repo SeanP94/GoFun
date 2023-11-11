0be7ee340f2d890571d4cf06fc482819627c4ff3
Sean <seanep94@gmail.com>
2023-11-10 22:49:55 -0800

    Reformatted some stuff, relearning basics of GO

---
+++ relearningnov/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const PI = 3.14159
+
+func main() {
+	fmt.Println("Hello World")
+	myMom := "FAT"
+	fmt.Println(myMom)
+	x := 15
+	y := 16
+	fmt.Printf("Is it %-4d?, nah it's %#x\n", x, y)
+}
