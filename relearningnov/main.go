654423bdfa9fa1e4feffdaa59f7a15ee5b6a7c46
Sean <seanep94@gmail.com>
2023-11-10 23:42:51 -0800

    More work, csv learning woo

--- relearningnov/main.go
+++ relearningnov/main.go
@@ -1,14 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+)
 
-const PI = 3.14159
+// readCsv reads in a fileName and returns the file in a [][]string format.
+func readCsv(fn string) [][]string {
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Fatal("You gave me the wrong file idiot.")
+	}
+	defer f.Close() // Make sure you close the file.
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Idk why but its not working.")
+	}
+	return records
+}
+
+// Inserts a number between 0-4 into a slice.
+func randInsert(myArr *[]int) {
+	*myArr = append(*myArr, rand.Intn(5))
+}
 
 func main() {
-	fmt.Println("Hello World")
-	myMom := "FAT"
-	fmt.Println(myMom)
-	x := 15
-	y := 16
-	fmt.Printf("Is it %-4d?, nah it's %#x\n", x, y)
+	myArr := []int{}
+	for i := 0; i < 5; i++ {
+		randInsert(&myArr)
+	}
+	fmt.Println(myArr)
+	for i, val := range myArr {
+		fmt.Printf(" %-2d: %d\n", i, val)
+	}
+	for _, row := range readCsv("test.csv") {
+		fmt.Println(row)
+	}
 }
