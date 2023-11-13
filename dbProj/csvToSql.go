02a83ad11bf5ca660f1c284dfb3dacdfb21e18f6
seanperry <seanep94@gmail.com>
2023-11-12 19:08:19 -0800

    Made the map more of a SQL syntax, and cleaned up the class

--- dbProj/csvToSql.go
+++ dbProj/csvToSql.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
+	"strings"
 )
 
 /*
@@ -58,42 +60,43 @@ func readCsv(fn string) [][]string {
 
 type csvToSql struct {
 	columnTypeMap map[string]string // Used to store the strings of the column types
-	csvData       [][]string
+	rowData       [][]string
+	columns       []string
 }
 
-// parseRows: takes in a [][]string csvData object. Creates the columnTypeMap for the csvToSql class.
+// parseRows: takes in a [][]string rowData object. Creates the columnTypeMap for the csvToSql class.
 // utilizes regex to map either string, int, or float to each column.
 func (c csvToSql) parseRows(csv [][]string) {
-	c.csvData = csv
-	// Used to add to map, and used as a key map for determining datatype.
-	columns := csv[0][:]
+	// Set our class variables (TODO, in future "constructor" move these lines.)
+	c.rowData = csv[1:][:]
+	c.columns = csv[0][:]
 	c.columnTypeMap = map[string]string{}
 
 	// Add items to the map
-	for _, column := range columns {
+	for _, column := range c.columns {
 		_, ok := c.columnTypeMap[column]
 		if ok {
 			log.Fatalf("Duplicate column name %v found in columns. Please correctRows", column)
 		}
-		c.columnTypeMap[column] = "string"
+		c.columnTypeMap[column] = "TEXT"
 	}
 
 	intMatch := regexp.MustCompile(`^-*[1-9]+[0-9]*$`)
-	floatMatch := regexp.MustCompile(`^-*[1-9]+[0-9]*\\.[1-9]+[0-9]*$`)
+	floatMatch := regexp.MustCompile(`^-{0,1}[1-9]+[0-9]*\.[0-9]+`)
 
-	for _, row := range c.csvData[1:][:] {
+	for _, row := range c.rowData {
 		for i, valueStr := range row {
-			colKey := columns[i]
+			colKey := c.columns[i]
 			value := []byte(valueStr)
 
 			// Match the strings with regex to see if they fit a parameter
 			foundInt := intMatch.Match([]byte(value))
 			foundFloat := floatMatch.Match(value)
 
-			if foundInt && c.columnTypeMap[colKey] == "string" {
-				c.columnTypeMap[colKey] = "int"
+			if foundInt && c.columnTypeMap[colKey] == "TEXT" {
+				c.columnTypeMap[colKey] = "INT"
 			} else if foundFloat {
-				c.columnTypeMap[colKey] = "float"
+				c.columnTypeMap[colKey] = "NUMERIC(5, 2)"
 			}
 
 			// TODO: Dates later
@@ -101,4 +104,18 @@ func (c csvToSql) parseRows(csv [][]string) {
 		}
 	}
 	fmt.Println(c.columnTypeMap)
+}
+
+func (c csvToSql) createTable(tableName string, primaryKey string) string {
+	pk := slices.Contains(c.rowData[0][:], primaryKey)
+	if !pk {
+		log.Fatalf("%v cannot be a primary key, as it is not a column.", primaryKey)
+	}
+	statement := []string{"CREATE TABLE [IF NOT EXISTS]" + tableName + "("}
+
+	// for i, column := range c.columns {
+
+	// }
+
+	return strings.Join(statement, "\n")
 }
