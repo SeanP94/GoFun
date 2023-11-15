89b3adaa652a640bbc6739132c341844897c4c1f
seanperry <seanep94@gmail.com>
2023-11-14 20:34:00 -0800

    Added a struct print for csvToSql and cleaned up main. Next, either Goroutines or  API(gin?)

--- dbProj/csvToSql.go
+++ dbProj/csvToSql.go
@@ -11,40 +11,9 @@ import (
 	"strings"
 )
 
-/*
-PROJECT TODO:
-	Build a csv -> SQL parser.
-	--- --- --- --- --- --- ---
-	Build a struct that is meant for you to parse csv files into Postgresql.
-
-	Notes:
-
-		readInCsv() # This should happen at the creation of the table.....
-		# Reads in a csv file and gets all data types mapped.
-		- I believe csv files read out as string or at least that's the format Id like to follow.
-		  - Parse lines utilize regex (Look into golang regex funs)
-		  - $##.##, ##.##.... float32
-		  - ##... ##,###.... $###.... int
-		  - ##/##/## Date ? (Look into this so you can val == a date type, or pass parameter that column is datetype )
-		  - Else string.
-
-		generateTable()
-		# Creates a Create Table string with parameters saved.
-		- Pass in table name, unique id, array of nonNullables.
-
-		printStruct()
-		# prints a string in the terminal to copy/paste the module struct.
-		- Not 100% if this is needed, but assuming if the table is created we might need to have a struct module for
-		  database migration/safe querying.
-
-		loadTable()
-		# Should take a db object, loads all the saved items in the table that was generated in readInCsv()
-
-		Notes:
-			Maybe have some simple query globals ready to test??
-*/
-
 // Helper Functions //
+
+// Reads a file name for a csv file and runs it and returns a [][]string
 func readCsv(fn string) [][]string {
 	f, err := os.Open(fn)
 	if err != nil {
@@ -147,4 +116,16 @@ func (c csvToSql) insertQueryStatement() string {
 	statement[len(statement)-1] = strings.TrimRight(statement[len(statement)-1], ",")
 	vals[len(vals)-1] = strings.TrimRight(vals[len(vals)-1], ",")
 	return strings.Join(statement, "") + ")\nVALUES (" + strings.Join(vals, "") + ");"
+}
+
+// createTableStruct will print a formated struct that can be copy/pasted as a table.
+// (This is just because some tables can take a lot of typing to do)
+func (c csvToSql) createTableStruct() string {
+
+	remap := map[string]string{"INT": "int", "FLOAT4": "float32", "TEXT": "string"}
+	statement := []string{fmt.Sprintf("type %v struct {", c.tableName)}
+	for col, colType := range c.columnTypeMap {
+		statement = append(statement, fmt.Sprintf("\t%v\t%v", col, remap[colType]))
+	}
+	return strings.Join(statement, "\n") + "\n}"
 }
--- dbProj/main.go
+++ dbProj/main.go
@@ -9,23 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// type gameEntry struct {
-// 	rank         int
-// 	name         string
-// 	platform     string
-// 	year         int
-// 	genre        string
-// 	publisher    string
-// 	na_sales     float32
-// 	eu_sales     float32
-// 	jp_sales     float32
-// 	other_sales  float32
-// 	global_sales float32
-// }
-
 const CONN_STRING string = "postgresql://postgres:dockerpw123@localhost:5432/searchEngineTest?sslmode=disable"
-
-// twoPointFloat Converts what was as string into a guaranteed .2f
 
 func createTable(db *sql.DB, query string) {
 	_, err := db.Exec(query)
@@ -35,6 +19,43 @@ func createTable(db *sql.DB, query string) {
 	fmt.Println("Table created successfully.")
 }
 
+func insertData(db *sql.DB, table csvToSql) {
+	// Inserts the data by row test.
+	for _, row := range table.rowData {
+
+		// These two are specific for personTable.
+		age, err := strconv.Atoi(row[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		networth, err := strconv.ParseFloat(row[3], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = db.Exec(table.insertStatement, row[0], age, row[2], networth)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func selectData(db *sql.DB, table csvToSql) {
+	var test string
+
+	query := `
+		SELECT name
+		FROM people_simple
+		LIMIT 1;
+	`
+	err := db.QueryRow(query).Scan(&test)
+
+	if err != nil {
+		log.Fatal("Failed to select person")
+	}
+	fmt.Println(test)
+}
+
 func main() {
 	// ^^Above code has been used for learning. Do not delete until progress is a bit more consistent. ^^ //
 	// Connect to db.
@@ -42,8 +63,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer db.Close()
+
 	// Check if DB is connected properly.
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
@@ -53,36 +74,9 @@ func main() {
 
 	personTable.csvToSqlInit("data/test.csv", "people")
 	createTableQuery := personTable.createTable("name")
-	fmt.Println(createTableQuery)
+
+	// Tests (For learning)
 	createTable(db, createTableQuery)
-
-	for _, row := range personTable.rowData {
-		age, err := strconv.Atoi(row[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-		networth, err := strconv.ParseFloat(row[3], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(networth)
-		_, err = db.Exec(personTable.insertStatement, row[0], age, row[2], networth)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	var test string
-
-	query := `
-		SELECT name
-		FROM people_simple
-		LIMIT 1;
-	`
-	err = db.QueryRow(query).Scan(&test)
-
-	if err != nil {
-		log.Fatal("Failed to select person")
-	}
-	fmt.Println(test)
+	insertData(db, personTable)
+	selectData(db, personTable)
 }
